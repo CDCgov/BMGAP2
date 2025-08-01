e809e14192f2d413d44956da7ec578054fd54b3f
Lee Katz - Aspen <gzu2@cdc.gov>
2025-08-01 10:29:19 -0400

    break up one liner

--- analysis_scripts/Alignment.sh
+++ analysis_scripts/Alignment.sh
@@ -11,8 +11,10 @@ NAME=${4}      #Sample name (M#)
 
 ###################################################
 # Remove human DNA
-bowtie2 -p 20 --local -t -x analysis_scripts/hg38/genome --un-conc-gz $OUT_DIR/${NAME}_R%_dedup_NoHuman.fastq.gz -1 $R1 -2 $R2 | \
-  samtools view -bhS - - > $OUT_DIR/${NAME}.human.bam
+bowtie2 -p 20 --local -t -x analysis_scripts/hg38/genome --un-conc-gz $OUT_DIR/${NAME}_R%_dedup_NoHuman.fastq.gz -1 $R1 -2 $R2 > \
+  $OUT_DIR/${NAME}.human.sam
+samtools view -bhS $OUT_DIR/${NAME}.human.sam > $OUT_DIR/${NAME}.human.bam
+rm -v $OUT_DIR/${NAME}.human.sam
 
 ###################################################
 # adapter, and quality trimming, filter for Phred quality 20
