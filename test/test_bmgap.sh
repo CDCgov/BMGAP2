5a325fc7f2c770a96a6c2071454eab500e101e85
Lee Katz - Aspen <gzu2@cdc.gov>
2025-08-12 09:30:00 -0400

    added test_bmgap.sh

---
+++ test/test_bmgap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bats
+# Test BMGAP results for a specific SRR which is exported in the environment
+# assume that conda environment or other environment is already activated
+
+# Check expected environment variables
+set -u
+echo "SRR: $SRR"
+echo "indir: $indir"
+echo "species_analysis_json: $species_analysis_json"
+echo "molecular_data_json: $molecular_data_json"
+echo "scheme_counts_json: $scheme_counts_json"
+echo "amr_data_json: $amr_data_json"
+
+# this directory is the directory that this script is in
+export thisDir=$(dirname "${BATS_TEST_FILENAME}")
+# The data for this SRR test is in $SRR.exp in the test folder
+export testDir="$thisDir/$SRR.exp"
+
+# test that jq is installed
+@test "jq is installed" {
+  run command -v jq
+  [ "$status" -eq 0 ]
+}
+
+# test that species is Neisseria meningitidis from the json
+@test "species_analysis" {
+  obs=$(jq --sort-keys . $species_analysis_json)
+  exp=$(jq --sort-keys . ${testDir}/species_analysis.json)
+  [[ "$obs" == "$exp" ]]
+}
