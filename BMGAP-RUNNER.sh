b24c482ac72468db50b6bea5857b714643d2ccf1
Lee Katz <gzu2@cdc.gov>
2026-02-11 15:15:25 -0500

    Updated SPAdes with --isolate flag and created run report with BMGAP … (#12)
    * Updated SPAdes with --isolate flag and created run report with BMGAP version
    
    * prefetch step
    
    * sidestep prefetch
    
    ---------
    
    Co-authored-by: Wiest <ujz6@nettie.biotech.cdc.gov>

--- BMGAP-RUNNER.sh
+++ BMGAP-RUNNER.sh
@@ -14,6 +14,11 @@ usage() {
     echo
 }
 
+# Script versioning (BMGAP Version, found in GitHub)
+SCRIPT_VERSION="2"
+SCRIPT_SUBVERSION="2.0"
+export BMGAP_VERSION="$SCRIPT_VERSION.$SCRIPT_SUBVERSION"
+
 # Check if no arguments or insufficient arguments are provided
 if [[ $# -lt 2 ]]; then
     usage
@@ -41,12 +46,23 @@ if ! ls "$FASTQ_DIR"/*.fastq.gz &>/dev/null; then
     exit 1
 fi
 
-echo "Current path: $PATH2"
-echo "FASTQ directory: $FASTQ_DIR"
-echo "Analysis directory: $ANALYSIS_DIRECTORY"
-echo "Analysis scripts directory: $ANALYSIS_SCRIPTS"
-
 mkdir -p $ANALYSIS_DIRECTORY
+###########################################
+
+# Prepare run report
+RUN_REPORT="$ANALYSIS_DIRECTORY/run_report.txt"
+START_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+
+{
+echo "Current path: $PATH2"
+echo "Run report generated: $START_TIME"
+echo "BMGAP Version: $SCRIPT_VERSION.$SCRIPT_SUBVERSION"
+echo "FASTQ input directory: $FASTQ_DIR"
+echo "Analysis output directory: $ANALYSIS_DIRECTORY"
+echo "Analysis scripts directory: $ANALYSIS_SCRIPTS"
+echo
+} > "$RUN_REPORT"
+
 LOG_DIR="$ANALYSIS_DIRECTORY/log"
 mkdir $LOG_DIR
 CTRL_FILE="$ANALYSIS_DIRECTORY/fastq.fofn"
@@ -59,7 +75,8 @@ echo ""
 for r1_file in "$FASTQ_DIR"/*R1*.fastq.gz; do
         #Check if file exists
 	if [[ ! -f $r1_file ]]; then
-		echo "R1 file not found: $r1_file" >&2
+		echo "No R1 files found (expected: $r1_file) ."
+		        echo "No R1 files found in $FASTQ_DIR, please use a different directory" >> "$RUN_REPORT"
 		exit 1
 	fi
 	
@@ -68,55 +85,51 @@ for r1_file in "$FASTQ_DIR"/*R1*.fastq.gz; do
        	
 	#Check if the corresponding R2 file exists
 	if [[ -f $r2_file ]]; then
-		echo -e "$r1_file\t$r2_file"
+		#Extract base name without path
+		base_name=$(basename "$r1_file" .fastq.gz)
+		base_name=${base_name%%_*} #Gets only M#
+		OUTPUT_DIR="$ANALYSIS_DIRECTORY/$base_name"
+		
+		# Define error and output file paths for isolate
+		error_file="$OUTPUT_DIR/${base_name}_error.e"
+		output_file="$OUTPUT_DIR/${base_name}_output.o"
+		
+		# Align PE-FASTQ files and remove human DNA
+		echo "Beginning alignment of $base_name."
+		align_id=$(qsub -V -e "$error_file" -o "$output_file" $ANALYSIS_SCRIPTS/Alignment.sh "$r1_file" "$r2_file" "$OUTPUT_DIR" "$base_name" | awk '{print $3}')
+		
+		# Assembly cleanup, check QC 
+		cleanup_id=$(qsub -V -hold_jid "$align_id" -e "$error_file" -o "$output_file" $ANALYSIS_SCRIPTS/cleanupSingle.sh "$OUTPUT_DIR" "$base_name" | awk '{print $3}')
+		echo "Submitted AssemblyCleanup, waiting for Alignment job $align_id to complete before beginning AssemblyCleanup."
+		
+		# Run BMScan on each new fasta file
+		bmscan_id=$(qsub -V -hold_jid "$cleanup_id" -e "$error_file" -o "$output_file" $ANALYSIS_SCRIPTS/BMScan.sh "$OUTPUT_DIR" "$base_name" | awk '{print $3}')
+		
+		# Run PMGA on each new fasta file 
+		pmga_id=$(qsub -V -hold_jid "$bmscan_id" -e "$error_file" -o "$output_file" $ANALYSIS_SCRIPTS/PMGA.sh "$OUTPUT_DIR" "$base_name" | awk '{print $3}')
+		sleep 200
+		
+		# Run LocusExtractor on each new fasta file
+		le_id=$(qsub -V -hold_jid "$bmscan_id" -e "$error_file" -o "$output_file" $ANALYSIS_SCRIPTS/LocusExtractor.sh "$OUTPUT_DIR" "$base_name" | awk '{print $3}')
+		echo "Submitted LocusExtractor, BMScan and PMGA, waiting for AssemblyCeanup job $cleanup_id to complete before running."
+		
+		# Run AMR with species code for each sample
+		amr_id=$(qsub -V -hold_jid "$pmga_id" -e "$error_file" -o "$output_file" $ANALYSIS_SCRIPTS/AMR.sh "$OUTPUT_DIR" "$base_name" | awk '{print $3}')
+		echo "Submitted AMR, waiting for PMGA job $pmga_id to complete before running."	
+		echo "Sample $base_name submitted.  Results will be in: $OUTPUT_DIR." >> "$RUN_REPORT"
+		sleep 200
+	else
+		echo "Warning: corresponding R2 file not found for $r1_file"
+		echo "Warning: R2 missing for $r1_file" >> "$RUN_REPORT"
 	fi
-done > $CTRL_FILE
+done 
 
-# Use -tc 2 to limit the number of concurrent jobs to 2 especially for pubmlst interactions
-qsub -N bmgap -o $ANALYSIS_DIRECTORY/log -j y -V -cwd -tc 2 -t 1-$(cat $CTRL_FILE | wc -l) \
-  -v "ANALYSIS_SCRIPTS=$ANALYSIS_SCRIPTS" -v "ANALYSIS_DIRECTORY=$ANALYSIS_DIRECTORY" -v "CTRL_FILE=$CTRL_FILE" <<- "END_OF_SCRIPT"
+END_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 
-	r1_file=$(sed -n ${SGE_TASK_ID}p $CTRL_FILE | awk '{print $1}')
-	r2_file=$(sed -n ${SGE_TASK_ID}p $CTRL_FILE | awk '{print $2}')
-
-	#Extract base name without path
-	base_name=$(basename "$r1_file" .fastq.gz)
-	base_name=${base_name%%_*} #Gets only M#
-	OUTPUT_DIR="$ANALYSIS_DIRECTORY/$base_name"
-	mkdir -p "$OUTPUT_DIR"
-	
-	# Define error and output file paths for isolate
-	error_file="$OUTPUT_DIR/${base_name}_error.e"
-	output_file="$OUTPUT_DIR/${base_name}_output.o"
-	
-	# Align PE-FASTQ files and remove human DNA
-	echo "Alignment of $base_name." | tee --append "$output_file" "$error_file"
-	bash $ANALYSIS_SCRIPTS/Alignment.sh "$r1_file" "$r2_file" "$OUTPUT_DIR" "$base_name" \
-		>> "$output_file" 2>> "$error_file"
-	
-	# Assembly cleanup, check QC 
-	echo "AssemblyCleanup for $base_name." | tee --append "$output_file" "$error_file"
-	$ANALYSIS_SCRIPTS/cleanupSingle.sh "$OUTPUT_DIR" "$base_name" \
-		>> "$output_file" 2>> "$error_file"
-	
-	# Run BMScan on each new fasta file
-	echo "BMSCAN for $base_name." | tee --append "$output_file" "$error_file"
-	bash $ANALYSIS_SCRIPTS/BMScan.sh "$OUTPUT_DIR" "$base_name" \
-		>> "$output_file" 2>> "$error_file"
-	
-	# Run PMGA on each new fasta file 
-	echo "PMGA for $base_name." | tee --append "$output_file" "$error_file"
-	bash $ANALYSIS_SCRIPTS/PMGA.sh "$OUTPUT_DIR" "$base_name" \
-		>> "$output_file" 2>> "$error_file"
-	
-	# Run LocusExtractor on each new fasta file
-	echo "LocusExtractor for $base_name." | tee --append "$output_file" "$error_file"
-	bash $ANALYSIS_SCRIPTS/LocusExtractor.sh "$OUTPUT_DIR" "$base_name" \
-		>> "$output_file" 2>> "$error_file"
-	
-	# Run AMR with species code for each sample
-	echo "AMR for $base_name." | tee --append "$output_file" "$error_file"
-	bash $ANALYSIS_SCRIPTS/AMR.sh "$OUTPUT_DIR" "$base_name" \
-		>> "$output_file" 2>> "$error_file"
-	
-END_OF_SCRIPT
+{
+echo
+echo "Run start time: $START_TIME"
+echo "Run end time: $END_TIME"
+echo "Full results directory: $ANALYSIS_DIRECTORY"
+echo "Note: Individual sample output directories are under the analysis directory."
+} >> "$RUN_REPORT"
